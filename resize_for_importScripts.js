aafad3de5d1cac9849d6a3a7233da97c3c304346
programingjd <programingjd@gmail.com>
2024-10-24 23:53:37 +0200

    Update dependencies and scripts.

--- resize_for_importScripts.js
+++ resize_for_importScripts.js
@@ -1,29 +1,32 @@
 const resize=(async()=>{
+  let wasm;
   const imports={
     wbg:{
-      __wbg_log_23c49b2eaab74338:(p,n)=>{
+      __wbg_log_bf8111044df980a8:(p,n)=>{
         console.log(new TextDecoder().decode(new Uint8Array(wasm.memory.buffer).subarray(p,p+n)));
+      },
+      __wbindgen_init_externref_table:function(){
+        const table=wasm.__wbindgen_export_0;
+        const offset=table.grow(4);
+        table.set(0);
+        table.set(offset);
+        table.set(offset+1,null);
+        table.set(offset+2,true);
+        table.set(offset+3,false);
       }
     }
   };
-  const {instance: {exports: wasm}}=await WebAssembly.instantiateStreaming(await fetch('./resize.wasm',{cache: 'force-cache'}),imports);
+  const {instance}=await WebAssembly.instantiateStreaming(await fetch('./resize.wasm',{cache: 'force-cache'}),imports);
+  wasm=instance.exports;
   const malloc=wasm.__wbindgen_malloc;
   const free=wasm.__wbindgen_free;
-  const pointer=wasm.__wbindgen_add_to_stack_pointer;
   return (data,sourceWidth,sourceHeight,targetWidth,targetHeight,hq=true)=>{
     const n1=data.length;
     const p1=malloc(n1,1);
-    const r=pointer(-16);
-    try{
-      new Uint8Array(wasm.memory.buffer).set(data,p1);
-      wasm.resize(r,p1,n1,sourceWidth,sourceHeight,targetWidth,targetHeight,hq);
-      const arr=new Int32Array(wasm.memory.buffer);
-      const p2=arr[r/4];const n2=arr[r/4+1];
-      const res=new Uint8Array(wasm.memory.buffer).subarray(p2,p2+n2).slice();
-      free(p2,n2);
-      return res;
-    }finally{
-      pointer(16);
-    }
+    new Uint8Array(wasm.memory.buffer).set(data,p1);
+    const [p2,n2]=wasm.resize(p1,n1,sourceWidth,sourceHeight,targetWidth,targetHeight,hq);
+    const res=new Uint8Array(wasm.memory.buffer).subarray(p2,p2+n2).slice();
+    free(p2,n2);
+    return res;
   };
 })();
